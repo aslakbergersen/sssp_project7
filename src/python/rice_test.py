57e7733725fbde7da3892d5b4fc426940108c2cd
Aslak Bergersen <aslak.bergersen@gmail.com>
2017-08-01 17:18:31 +0200

    A working version which one can use to investigate 'all' problems.

--- src/python/rice_test.py
+++ src/python/rice_test.py
@@ -1,119 +1,187 @@
-import rice_model_2008_new_dir as rice
 from scipy.integrate import odeint
 from scipy.optimize import fsolve
+from argparse import ArgumentParser
 import math
 from math import exp
+import os
 import numpy as np
 import pylab
 
 
 def read_command_lines():
+    decription = """This is the main program for our project. All of us can
+    probolay produce results for our individual tasks from this!"""
 
-    return args
+    parser = ArgumentParser(description=decription)
 
+    # Required arguments
+    require = parser.add_argument_group("required named arguments")
 
-def pasive_tension_usysk(lambda_):
-    e11 = 0.5 * (lambda_**2 - 1)
-    e22 = 0.5 * (1/lambda_ - 1)
-    W = bff*e11**2 + bxx*(e22**2+e22**2)
-    T_p = 0.5*K*bff*(lambda_**2-1.)*exp(W)
+    # Option arguments
+    parser.add_argument("-s", "--solid_model", default="holzapfel", type=str,
+                                                choices=["holzapfel", "usysk",
+                                                         "zero-pole"],
+                        help="Type of solid model.")
+    parser.add_argument("-c", "--cell_model", default="rice", type=str,
+                        choices=["rice"], help="Type of cell model, for now only rice is implemented")
+    parser.add_argument("-t", "--dt", default=1, type=float, help="Timestep size")
+    parser.add_argument("-T", "--time", default=1000, type=float, help="End time in [ms]")
+    parser.add_argument("-N", "--number_of_timesteps", default=None, type=int,
+                       help="Number of time steps")
+    parser.add_argument("-r", "--step", default=10, type=int, help="Number of cell solves for one solid solve")
+    parser.add_argument("-C", "--coupling", default="FE", type=str,
+                        choices=["FE", "all", "xSL", "GRL"],
+                       help="Different types of couplings between the cell model and solid model, cf. Sundnes et al. 2014 for a more thourgh description.")
 
-    return T_p
+    args = parser.parse_args()
+    if args.number_of_timesteps is not None:
+        dt = args.time/float(args.number_of_timesteps)
+    else:
+        dt = args.dt
 
-
-def pasive_tension_pole_zero(lambda_):
-    # Pole-Zero Mechanics model
-    e11 = 0.5 * (lambda_**2 - 1)
-    e22 = 0.5 * (1/lambda_ - 1)
-    T_p = k1 * e11/(a1 - e11)**(b1) * (2 + (b1*e11)/(a1 - e11))
-    T_p += -2*k2 * e22/(a2 - e22)**(b2) * (2 + (b2*e22)/(a2 - e22))
-
-    return T_p
-
-
-def pasive_tension_holzapfel(lambda_):
-    #Holzapfel mechanics model
-    c11 = lambda_**2
-    c22 = 1./lambda_
-    I1 = c11 + 2.*c22
-    I4f = c11
-
-    T_p = (a/2.)*exp(b*(I1-3.)) + af*exp(bf*(I4f-1.)**2)*(I4f-1.)
-
-    return T_p
-
-
-def active_tension_FE(lambda_):
-    xXBprer = xXBprer_prev + (dt)*(#0.5*dSL + \
-                0.5*SL0*(lambda_ - lambda_prev)/(dt) + \
-                phi / dutyprer * (-fappT*xXBprer_prev + hbT*(xXBpostr_prev - \
-                x_0 - xXBprer_prev)))
-
-    xXBpostr = xXBpostr_prev + (dt)* (#0.5*dSL + \
-                0.5*SL0*(lambda_ - lambda_prev)/(dt) + \
-                phi / dutypostr * (hfT*(xXBprer_prev + x_0 - xXBpostr_prev)))
-
-    tension = SOVFThick*(XBprer_prev*xXBprer+XBpostr_prev*xXBpostr) / (x_0 * SSXBpostr)
-
-    return tension
+    return args.solid_model, args.cell_model, args.number_of_timesteps, dt, \
+            args.step, args.time, args.coupling
 
 
-def active_tension_all(lambda_):
-    p = (rice.init_parameter_values(dSL=SL0*(lambda_- lambda_prev )/dt),)
-    init = rice.init_state_values(SL=SL0*lambda_,
-                                      intf=intf_prev,
-                                      TRPNCaH=TRPNCaH_prev,
-                                      TRPNCaL=TRPNCaL_prev, N=N_prev, N_NoXB=N_NoXB_prev,
-                                      P_NoXB=P_NoXB_prev, XBpostr=XBpostr_prev,
-                                      XBprer=XBprer_prev,
-                                      xXBpostr=xXBpostr_prev,
-                                      xXBprer=xXBprer_prev)
+#FIXME: The functions are defined within the main function in order to have
+#       access to variables inside the functions. This is not an elegant
+#       solution, but a solution.
+def main(T, N, dt, step, solid_model, coupling, lambda_prev=1, dldt=0):
+    # Other possible standard choises for lambda and dldt
+    # lambda_prev = 0.9663
+    # dldt = SL0 * (lambda_ - lambda_prev) / dt
 
-    ss = odeint(rice.rhs, init, [t, t+dt], p)
+    def pasive_tension_usysk(lambda_):
+        e11 = 0.5 * (lambda_**2 - 1)
+        e22 = 0.5 * (1/lambda_ - 1)
+        W = bff*e11**2 + bxx*(e22**2+e22**2)
+        T_p = 0.5*K*bff*(lambda_**2-1.)*exp(W)
 
-    SL_prev1, intf_prev1, TRPNCaH_prev1, TRPNCaL_prev1, N_prev1, N_NoXB_prev1, \
-    P_NoXB_prev1, XBpostr_prev1, XBprer_prev1, xXBpostr_prev1, xXBprer_prev1 = ss[-1]
+        return T_p
 
-    mm = rice.monitor(s[-1], t+dt, p[0])
-    SOVFThick1 = mm[SOVFThick_index]
-    SSXBpostr1 = mm[SSXBpostr_index]
+    def pasive_tension_pole_zero(lambda_):
+        # Pole-Zero Mechanics model
+        e11 = 0.5 * (lambda_**2 - 1)
+        e22 = 0.5 * (1/lambda_ - 1)
+        T_p = k1 * e11/(a1 - e11)**(b1) * (2 + (b1*e11)/(a1 - e11))
+        T_p += -2*k2 * e22/(a2 - e22)**(b2) * (2 + (b2*e22)/(a2 - e22))
 
-    tension = SOVFThick1*(XBprer_prev1*xXBprer_prev1+XBpostr_prev1*xXBpostr_prev1) / (x_0 * SSXBpostr1)
+        return T_p
 
-    return tension
+    def pasive_tension_holzapfel(lambda_):
+        #Holzapfel mechanics model
+        c11 = lambda_**2
+        c22 = 1./lambda_
+        I1 = c11 + 2.*c22
+        I4f = c11
+
+        T_p = (a/2.)*exp(b*(I1-3.)) + af*exp(bf*(I4f-1.)**2)*(I4f-1.)
+
+        return T_p
+
+    def active_tension_FE(lambda_):
+        xXBprer = xXBprer_prev + (dt)*(0.5*SL0*(lambda_ - lambda_prev)/(dt) + \
+                    phi / dutyprer * (-fappT*xXBprer_prev + hbT*(xXBpostr_prev - \
+                    x_0 - xXBprer_prev)))
+
+        xXBpostr = xXBpostr_prev + (dt)* (0.5*SL0*(lambda_ - lambda_prev)/(dt) + \
+                    phi / dutypostr * (hfT*(xXBprer_prev + x_0 - xXBpostr_prev)))
+
+        tension = SOVFThick*(XBprer_prev*xXBprer+XBpostr_prev*xXBpostr) / (x_0 * SSXBpostr)
+
+        return tension
+
+    # TODO: There is something wrong with how the update of prev1 is implemented
+    # inside the function
+    def active_tension_all(lambda_):
+        p = (rice.init_parameter_values(dSL=SL0*(lambda_- lambda_prev )/dt),)
+        init = rice.init_state_values(SL=SL0*lambda_,
+                                          intf=intf_prev,
+                                          TRPNCaH=TRPNCaH_prev,
+                                          TRPNCaL=TRPNCaL_prev, N=N_prev, N_NoXB=N_NoXB_prev,
+                                          P_NoXB=P_NoXB_prev, XBpostr=XBpostr_prev,
+                                          XBprer=XBprer_prev,
+                                          xXBpostr=xXBpostr_prev,
+                                          xXBprer=xXBprer_prev)
+
+        ss = odeint(rice.rhs, init, [t, t+dt], p)
+
+        SL_prev1, intf_prev1, TRPNCaH_prev1, TRPNCaL_prev1, N_prev1, N_NoXB_prev1, \
+        P_NoXB_prev1, XBpostr_prev1, XBprer_prev1, xXBpostr_prev1, xXBprer_prev1 = ss[-1]
+
+        mm = rice.monitor(s[-1], t+dt, p[0])
+        SOVFThick1 = mm[SOVFThick_index]
+        SSXBpostr1 = mm[SSXBpostr_index]
+
+        tension = SOVFThick1*(XBprer_prev1*xXBprer_prev1+XBpostr_prev1*xXBpostr_prev1) / (x_0 * SSXBpostr1)
+
+        return tension
+
+    def active_tension_xSL(lambda_):
+        xSL = 0.5 * SL0 * (lambda_ - 1)
+        xXB_prer = dt*phi/dutyprer*fappT*xSL + xSL
+
+    def active_tension_GRL(lambda_):
+        a_prer = 0.5 * (lambda_ - lambda_prev)/dt + (phi/dutyprer)*(-fappT*xXBprer_prev + hbT*(xXBpostr_prev -x_0 -xXBprer_prev) )
+        a_postr = 0.5 * (lambda_ - lambda_prev)/dt + (phi/dutypostr)*( hfT*(xXBprer_prev + x_0 - xXBpostr_prev) )
+        b_prer = -phi*(fappT+hbT)/dutyprer
+        b_postr = -phi*hfT/dutypostr
+
+        xXBprer = xXBprer_prev + (a_prer/b_prer)*( exp(b_prer*dt) - 1.0 )
+        xXBpostr = xXBpostr_prev + (a_postr/b_postr)*( exp(b_postr*dt) - 1.0 )
+
+        tension = SOVFThick*(XBprer_prev*xXBprer+XBpostr_prev*xXBpostr) / (x_0 * SSXBpostr)
+
+        return tension
+
+    def f(lambda_):
+        tension = active_tension(lambda_)
+        T_p = pasive_tension(lambda_)
+        T_p += tension*force_scale
+
+        return T_p
+
+    # Parameters for Holzapfel mechanics model
+    if solid_model == "holzapfel":
+        a = 0.057
+        b = 8.094
+        af = 21.503
+        bf = 15.819
+        force_scale = 2000
+        pasive_tension = pasive_tension_holzapfel
+
+    # Parameters for Usysk mechanics model
+    elif solid_model == "usysk":
+        bff = 20
+        bxx = 4
+        K = 0.876
+        force_scale = 200
+        pasive_tension = pasive_tension_usysk
+
+    # Parameters for zero-pole machincs model
+    elif solid_model == "zero-pole":
+        a1 = 0.475
+        a2 = 0.619
+        b1 = 1.5
+        b2 = 1.2
+        k1 = 2.22
+        k2 = 2.22
+        force_scale = 200
+        pasive_tension = pasive_tension_pole_zero
 
 
-def active_tension_xSL(lambda_):
-    xSL = 0.5 * SL0 * (lambda_ - 1)
-    xXB_prer = dt*phi/dutyprer*fappT*xSL + xSL
+    if coupling == "FE":
+        active_tension = active_tension_FE
+    if coupling == "xSL":
+        # TODO: Not implemented
+        active_tension = active_tension_xSL
+    if coupling == "GRL":
+        active_tension = active_tension_GRL
+    if coupling == "all":
+        active_tension = active_tension_all
 
-
-def active_tension_GRL(lambda_):
-    a_prer = 0.5 * (lambda_ - lambda_prev)/dt + (phi/dutyprer)*(-fappT*xXBprer_prev + hbT*(xXBpostr_prev -x_0 -xXBprer_prev) )
-    a_postr = 0.5 * (lambda_ - lambda_prev)/dt + (phi/dutypostr)*( hfT*(xXBprer_prev + x_0 - xXBpostr_prev) )
-    b_prer = -phi*(fappT+hbT)/dutyprer
-    b_postr = -phi*hfT/dutypostr
-
-    xXBprer = xXBprer_prev + (a_prer/b_prer)*( exp(b_prer*dt) - 1.0 )
-    xXBpostr = xXBpostr_prev + (a_postr/b_postr)*( exp(b_postr*dt) - 1.0 )
-
-    tension = SOVFThick*(XBprer_prev*xXBprer+XBpostr_prev*xXBpostr) / (x_0 * SSXBpostr)
-
-    return tension
-
-
-def f(lambda_):
-    tension = active_tension(lambda_)
-    T_p = pasive_tension(lambda_)
-    T_p += tension*force_scale
-
-    return T_p
-
-
-def main(T, N, dt, step, global_time, lambda_prev=1, dldt=0):
-    # Initial values of mechanics model to the cell model 
-    #lambda_prev = 1 #0.9663
-    #dldt = 0 #SL0 * (lambda_ - lambda_prev) / dt
+    # Global time steps
+    global_time = np.linspace(0, T, N+1)
 
     SOVFThick_index = rice.monitor_indices("SOVFThick")
     SSXBpostr_index = rice.monitor_indices("SSXBpostr")
@@ -138,8 +206,7 @@ def main(T, N, dt, step, global_time, lambda_prev=1, dldt=0):
         # Set initial values
         t_local = np.linspace(t, global_time[i+1], step+1)
         if i == 0:
-            #p = (rice.init_parameter_values(dSL=dldt),)
-            p = (rice.init_parameter_values(), )
+            p = (rice.init_parameter_values(dSL=dldt),)
             init = rice.init_state_values()
         else:
             p = (rice.init_parameter_values(dSL=dldt),)
@@ -184,116 +251,82 @@ def main(T, N, dt, step, global_time, lambda_prev=1, dldt=0):
         t_list.append(t_local[-1])
         dldt_list.append(dldt)
 
-        return l_list, Ta_list, t_list, dldt_list
+    return l_list, Ta_list, t_list, dldt_list
 
 
 def postprosess(l_list, Ta_list, t_list, dldt_list, cell_model, coupling,
-                solid_model):
+                solid_model, dt):
     rel_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "plot")
     if not os.path.exists(rel_path):
         os.makedirs(rel_path)
 
+    # Plot parameters
+    fontsize = 16
+    color = "k"
+    linewidth = 2
+
+    # TODO: Add a title for each plot
     pylab.figure(0)
-    pylab.plot(l_list, Ta_list)
-    pylab.xlabel("SL [$\mu m$]")
-    pylab.ylabel("Scaled normalied active force [-]")
+    pylab.plot(l_list, Ta_list, linewidth=linewidth, color=color)
+    pylab.xlabel("SL [$\mu m$]", fontsize=fontsize)
+    pylab.ylabel("Scaled normalied active force [-]", fontsize=fontsize)
     pylab.savefig(os.path.join(rel_path,
-                               "%s_%s_%s_sl_force.png" \
-                               % (cell_model, coupling, solid_model)))
+                               "%s_%s_%s_dt%f_sl_force.png" \
+                               % (cell_model, coupling, solid_model, dt)))
 
     pylab.figure(1)
-    pylab.plot(t_list,Ta_list)
-    pylab.ylabel("Scaled normalied active force [-]")
-    pylab.xlabel("Time [ms]")
+    pylab.plot(t_list, Ta_list, linewidth=linewidth, color=color)
+    pylab.ylabel("Scaled normalied active force [-]", fontsize=fontsize)
+    pylab.xlabel("Time [ms]", fontsize=fontsize)
     pylab.savefig(os.path.join(rel_path,
-                               "%s_%s_%s_force.png" \
-                               % (cell_model, coupling, solid_model)))
+                               "%s_%s_%s_dt%f_force.png" \
+                               % (cell_model, coupling, solid_model, dt)))
 
     pylab.figure(2)
-    pylab.plot(t_list,l_list)
-    pylab.ylabel("SL [$\mu m$]")
-    pylab.xlabel("Time [ms]")
+    pylab.plot(t_list, l_list, linewidth=linewidth, color=color)
+    pylab.ylabel("SL [$\mu m$]", fontsize=fontsize)
+    pylab.xlabel("Time [ms]", fontsize=fontsize)
     pylab.savefig(os.path.join(rel_path,
-                               "%s_%s_%s_sl.png" \
-                               % (cell_model, coupling, solid_model)))
+                               "%s_%s_%s_dt%f_sl.png" \
+                               % (cell_model, coupling, solid_model, dt)))
 
     pylab.figure(3)
-    pylab.plot(t_list, dldt_list)
-    pylab.ylabel("Shortening velocity [$\mu m/s$]")
-    pylab.xlabel("Time [ms]")
-    pylab.ylim([0.8, 1])
-    pylab.savefig(os.path.join(rel_path,e
-                                "%s_%s_%s_sl_velocity.png" \
-                                % (cell_model, coupling, solid_model)))
+    pylab.plot(t_list, dldt_list, linewidth=linewidth, color=color)
+    pylab.ylabel("Shortening velocity [$\mu m/s$]", fontsize=fontsize)
+    pylab.xlabel("Time [ms]", fontsize=fontsize)
+    pylab.savefig(os.path.join(rel_path,
+                                "%s_%s_%s_dt%f_sl_velocity.png" \
+                                % (cell_model, coupling, solid_model, dt)))
 
     pylab.figure(4)
-    pylab.plot(Ta_list, dldt_list)
-    pylab.xlabel("Force")
-    pylab.ylabel("Shortening velocity [$\mu m/s$]")
-    pylab.savefig(os.path.join(rel_path,e
-                                "%s_%s_%s_sl_velocity_force.png" \
-                                % (cell_model, coupling, solid_model)))
+    pylab.plot(Ta_list, dldt_list, linewidth=linewidth, color=color)
+    pylab.xlabel("Force", fontsize=fontsize)
+    pylab.ylabel("Shortening velocity [$\mu m/s$]", fontsize=fontsize)
+    pylab.savefig(os.path.join(rel_path,
+                                "%s_%s_%s_dt%f_sl_velocity_force.png" \
+                                % (cell_model, coupling, solid_model, dt)))
 
 
 
 if __name__ == "__main__":
-    solid_model, cell_model, dt, step, T, coupling
-
-    # Parameters for Holzapfel mechanics model
-    if solid_model == "holzapfel":
-        a = 0.057
-        b = 8.094
-        af = 21.503
-        bf = 15.819
-        force_scale = 2000
-        pasive_tension = pasive_tension_holzapfel
-
-    # Parameters for Usysk mechanics model
-    elif solid_model == "usysk":
-        bff = 20
-        bxx = 4
-        K = 0.876
-        force_scale = 200
-        pasive_tension = pasive_tension_usysk
-
-    # Parameters for zero-pole machincs model
-    elif solid_model == "zero-pole":
-        a1 = 0.475
-        a2 = 0.619
-        b1 = 1.5
-        b2 = 1.2
-        k1 = 2.22
-        k2 = 2.22
-        force_scale = 200
-        pasive_tension = pasive_tension_pole_zero
+    solid_model, cell_model, N, dt, step, T, coupling = read_command_lines()
 
     # Parameters for the cell model
+    if cell_model == "rice":
+        import rice_model_2008_new_dir as rice
     # TODO: For now only one cell model is implemented, but could and should be
     # extended in the future
     x_0 = 0.007
     phi = 2
     SL0 = 1.89999811516
-    force_scale = 2000 #200
-
-    if coupling == "FE":
-        active_tension = active_tension_FE
-    if coupling == "xSL":
-        # TODO: Not implemented
-        active_tension = active_tension_xSL
-    if coupling == "GRL":
-        active_tension = active_tension_GRL
-    if coupling == "all":
-        active_tension = active_tension_all
 
     # Time variables
-    T = 1000
     if N is None:
-        N = T/dt
-    elif dt is None:
-        dt = 1.*T/N
-    step = 1
-    global_time = np.linspace(0, T, N+1)
+        N = int(T/dt)
 
-    l_list, Ta_list, t_list, dldt_list = main(T, N, dt, step, global_time)
-    postprosess(l_list, Ta_list, t_list, dldt_list)
+    # Run the program
+    l_list, Ta_list, t_list, dldt_list = main(T, N, dt, step, solid_model, coupling)
 
+    # Post prosess
+    postprosess(l_list, Ta_list, t_list, dldt_list, cell_model,
+                coupling, solid_model, dt)
