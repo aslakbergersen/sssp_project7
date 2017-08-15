22ccdad23788e6a56ec6eb49324bb2d618d61525
Aslak Bergersen <aslak.bergersen@gmail.com>
2017-08-14 22:21:06 -0700

    Clean up for reproducibility

---
+++ src/python/run_FE.sh
@@ -0,0 +1,55 @@
+python run_rice.py -C FE --dt 2 -s holzapfel
+python run_rice.py -C FE --dt 1 -s holzapfel
+python run_rice.py -C FE --dt 0.5 -s holzapfel
+python run_rice.py -C FE --dt 0.25 -s holzapfel
+python run_rice.py -C FE --dt 0.1 -s holzapfel
+python run_rice.py -C FE --dt 0.05 -s holzapfel
+python run_rice.py -C FE --dt 0.025 -s holzapfel
+
+python run_rice.py -C FE --dt 2 -s usysk
+python run_rice.py -C FE --dt 1 -s usysk
+python run_rice.py -C FE --dt 0.5 -s usysk
+python run_rice.py -C FE --dt 0.25 -s usysk
+python run_rice.py -C FE --dt 0.1 -s usysk
+python run_rice.py -C FE --dt 0.05 -s usysk
+python run_rice.py -C FE --dt 0.025 -s usysk
+
+python run_rice.py -C FE --dt 2 -s holzapfel_inc
+python run_rice.py -C FE --dt 1 -s holzapfel_inc
+python run_rice.py -C FE --dt 0.5 -s holzapfel_inc
+python run_rice.py -C FE --dt 0.25 -s holzapfel_inc
+python run_rice.py -C FE --dt 0.1 -s holzapfel_inc
+python run_rice.py -C FE --dt 0.05 -s holzapfel_inc
+python run_rice.py -C FE --dt 0.025 -s holzapfel_inc
+
+python run_rice.py -C FE --dt 2 -s holzapfel_viscous_inc
+python run_rice.py -C FE --dt 1 -s holzapfel_viscous_inc
+python run_rice.py -C FE --dt 0.5 -s holzapfel_viscous_inc
+python run_rice.py -C FE --dt 0.25 -s holzapfel_viscous_inc
+python run_rice.py -C FE --dt 0.1 -s holzapfel_viscous_inc
+python run_rice.py -C FE --dt 0.05 -s holzapfel_viscous_inc
+python run_rice.py -C FE --dt 0.025 -s holzapfel_viscous_inc
+
+python run_rice.py -C FE --dt 2 -s usysk_inc
+python run_rice.py -C FE --dt 1 -s usysk_inc  
+python run_rice.py -C FE --dt 0.5 -s usysk_inc
+python run_rice.py -C FE --dt 0.25 -s usysk_inc
+python run_rice.py -C FE --dt 0.1 -s usysk_inc
+python run_rice.py -C FE --dt 0.05 -s usysk_inc
+python run_rice.py -C FE --dt 0.025 -s usysk_inc
+
+python run_rice.py -C FE --dt 2 -s pole-zero
+python run_rice.py -C FE --dt 1 -s pole-zero
+python run_rice.py -C FE --dt 0.5 -s pole-zero
+python run_rice.py -C FE --dt 0.25 -s pole-zero
+python run_rice.py -C FE --dt 0.1 -s pole-zero
+python run_rice.py -C FE --dt 0.05 -s pole-zero
+python run_rice.py -C FE --dt 0.025 -s pole-zero
+
+python run_rice.py -C FE --dt 2 -s pole-zero_inc
+python run_rice.py -C FE --dt 1 -s pole-zero_inc
+python run_rice.py -C FE --dt 0.5 -s pole-zero_inc
+python run_rice.py -C FE --dt 0.25 -s pole-zero_inc
+python run_rice.py -C FE --dt 0.1 -s pole-zero_inc
+python run_rice.py -C FE --dt 0.05 -s pole-zero_inc
+python run_rice.py -C FE --dt 0.025 -s pole-zero_inc
